548aae18cadef45c38d23d5e7086f2a83ea2524b
Rohit Pradhan <rohit.pradhan@fbwinn.com>
2023-12-08 12:43:01 +0530

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Rohit Pradhan on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
